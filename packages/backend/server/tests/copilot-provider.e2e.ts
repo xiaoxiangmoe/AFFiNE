9b62ebe1390090c841395d1144aa19c079f38217
DarkSky <darksky2048@gmail.com>
2024-11-08 12:21:51 +0800

    chore: skip api test if env not set

--- packages/backend/server/tests/copilot-provider.e2e.ts
+++ packages/backend/server/tests/copilot-provider.e2e.ts
@@ -32,7 +32,27 @@ const e2eConfig = {
   secret: process.env.COPILOT_E2E_SECRET || 'affine',
 };
 
+const isCopilotConfigured =
+  !!process.env.COPILOT_OPENAI_API_KEY &&
+  !!process.env.COPILOT_FAL_API_KEY &&
+  process.env.COPILOT_OPENAI_API_KEY !== '1' &&
+  process.env.COPILOT_FAL_API_KEY !== '1';
+const runIfCopilotConfigured = test.macro(
+  async (
+    t,
+    callback: (t: ExecutionContext<Tester>) => Promise<void> | void
+  ) => {
+    if (isCopilotConfigured) {
+      await callback(t);
+    } else {
+      t.log('Skip test because copilot is not configured');
+      t.pass();
+    }
+  }
+);
+
 test.before(async t => {
+  if (!isCopilotConfigured) return;
   const { endpoint, user, password, secret } = e2eConfig;
   const app = { getHttpServer: () => endpoint } as INestApplication<any>;
   const token = await automaticSignIn(app, user, password, secret);
@@ -92,60 +112,68 @@ const makeCopilotChat = async (
 for (const { promptName, messages, verifier, type } of ProviderActionTestCase) {
   const prompts = Array.isArray(promptName) ? promptName : [promptName];
   for (const promptName of prompts) {
-    test(`should be able to run action: ${promptName}`, async t => {
-      await retry(`action: ${promptName}`, t, async t => {
-        const { app, userToken } = t.context;
-        const { sessionId, messageId } = await makeCopilotChat(
-          t,
-          promptName,
-          messages[0]
-        );
-
-        if (type === 'text') {
-          const result = await chatWithText(
-            app,
-            userToken,
-            sessionId,
-            messageId
+    test(
+      `should be able to run action: ${promptName}`,
+      runIfCopilotConfigured,
+      async t => {
+        await retry(`action: ${promptName}`, t, async t => {
+          const { app, userToken } = t.context;
+          const { sessionId, messageId } = await makeCopilotChat(
+            t,
+            promptName,
+            messages[0]
           );
-          t.truthy(result, 'should return result');
-          verifier?.(t, result);
-        } else if (type === 'image') {
-          const result = sse2array(
-            await chatWithImages(app, userToken, sessionId, messageId)
-          )
-            .filter(e => e.event !== 'event')
-            .map(e => e.data)
-            .filter(Boolean);
-          t.truthy(result.length, 'should return result');
-          for (const r of result) {
-            verifier?.(t, r);
+
+          if (type === 'text') {
+            const result = await chatWithText(
+              app,
+              userToken,
+              sessionId,
+              messageId
+            );
+            t.truthy(result, 'should return result');
+            verifier?.(t, result);
+          } else if (type === 'image') {
+            const result = sse2array(
+              await chatWithImages(app, userToken, sessionId, messageId)
+            )
+              .filter(e => e.event !== 'event')
+              .map(e => e.data)
+              .filter(Boolean);
+            t.truthy(result.length, 'should return result');
+            for (const r of result) {
+              verifier?.(t, r);
+            }
+          } else {
+            t.fail('unsupported provider type');
           }
-        } else {
-          t.fail('unsupported provider type');
-        }
-      });
-    });
+        });
+      }
+    );
   }
 }
 
 // ==================== workflow ====================
 
 for (const { name, content, verifier } of ProviderWorkflowTestCase) {
-  test(`should be able to run workflow: ${name}`, async t => {
-    await retry(`workflow: ${name}`, t, async t => {
-      const { app, userToken } = t.context;
-      const { sessionId, messageId } = await makeCopilotChat(
-        t,
-        `workflow:${name}`,
-        { content }
-      );
-      const r = await chatWithWorkflow(app, userToken, sessionId, messageId);
-      const result = sse2array(r)
-        .filter(e => e.event !== 'event' && e.data)
-        .reduce((p, c) => p + c.data, '');
-      t.truthy(result, 'should return result');
-      verifier?.(t, result);
-    });
-  });
+  test(
+    `should be able to run workflow: ${name}`,
+    runIfCopilotConfigured,
+    async t => {
+      await retry(`workflow: ${name}`, t, async t => {
+        const { app, userToken } = t.context;
+        const { sessionId, messageId } = await makeCopilotChat(
+          t,
+          `workflow:${name}`,
+          { content }
+        );
+        const r = await chatWithWorkflow(app, userToken, sessionId, messageId);
+        const result = sse2array(r)
+          .filter(e => e.event !== 'event' && e.data)
+          .reduce((p, c) => p + c.data, '');
+        t.truthy(result, 'should return result');
+        verifier?.(t, result);
+      });
+    }
+  );
 }
